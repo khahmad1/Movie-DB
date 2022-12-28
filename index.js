c3249a19ba5107818e59d2aeaa55cc003e49466c
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-29 00:41:59 +0200

    step 4

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.get('/',  (req, res) => {
     var minutes = curretnime.getMinutes();
     res.json({status: 200, message: `${hours}:${minutes}` })
   });
+  app.get('/hello/:id', (req, res) => {
+    var ID = req.params.id;
+    res.status(200).json({ status: 200, message: `Hello, ${ID}` });
+});
+
+app.get("/search", (req, res) => {
+    var search = req.query.search
+    if (search) {
+        res.status(200).json({ status: 200, message: "ok", data: search })
+    } else {
+        res.status(500).json({ status: 500, error: true, message: "you have to provide a search" })
+    }
+})
 
  app.listen(3000, () => {
     console.log('Server on port 3000');
