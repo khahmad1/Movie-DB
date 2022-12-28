1c93babe28fe24a84a3fe107b7bf5463a7a21c13
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-28 19:30:56 +0200

    step 3

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.get('/',  (req, res) => {
     res.send('OK');
   });
 
+  app.get('/test',(req, res) => {
+    res.json({ status:200, message:"ok" })
+  });
+  app.get('/time',(req, res) => {
+    var  currenttime = new Date();
+    var hours = curretntime.getHours();
+    var minutes = curretnime.getMinutes();
+    res.json({status: 200, message: `${hours}:${minutes}` })
+  });
+
  app.listen(3000, () => {
     console.log('Server on port 3000');
   });
