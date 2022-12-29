c59b00dae7bc843805645757705761630fa77469
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-29 13:17:39 +0200

    step 6

--- index.js
+++ index.js
@@ -49,6 +49,25 @@ app.get('/movies/delete', (req, res) => {
   
 });
 
+app.get('/movies/read/by-date', (req, res) => {
+  var ByDate = movies.sort((x, y) => x.date - y.date);
+  res.json({ status: 200, data:ByDate });
+});
+
+app.get('/movies/read/by-rating', (req, res) => {
+  var ByRating = movies.sort((x, y) => y.rating - x.rating);
+  res.json({ status: 200, data:ByRating });
+});
+
+app.get('/movies/read/by-title', (req, res) => {
+  var ByTitle = movies.sort((x, y) => {
+    if (x.title < y.title) return -1;
+    if (x.title > y.title) return 1;
+    return 0;
+  });
+  res.json({ status: 200, data: ByTitle });
+});
+
  app.listen(3000, () => {
     console.log('Server on port 3000');
   });
