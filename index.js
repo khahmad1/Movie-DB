bfa280b9119298014afae8d98b939ead5d04f345
Khahmad1 <ahmad2003kharma@gmail.com>
2022-12-31 00:47:00 +0200

    step 10

--- index.js
+++ index.js
@@ -102,6 +102,18 @@ app.get("/movies/delete/:id", (req, res) => {
   }
 })
 
+app.get("/movies/edit/:id", (req, res) => {
+  var id = parseInt(req.params.id)
+  if (id > movies.length) {
+      res.status(404).json({ status: 404, error: true, message: `the movie ${id} does not exist` })
+  } else {
+      var movie = movies[id - 1]
+      if (req.query.title) movie.title = req.query.title
+      if (req.query.rating) movie.rating = req.query.rating
+      if (req.query.year) movie.year = req.query.year
+      res.status(200).json({ status: 200, data: movies })
+  }
+})
  app.listen(3000, () => {
     console.log('Server on port 3000');
   });
